05bedff8bde119d05165e5f903fe164154db7185
Ariel5862 <Ar0587411585@mail.com>
2025-08-17 14:13:04 +0300

    clear not relavent code from app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import MovieCard from "./MovieCard";
 
 function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-black p-4 rounded-lg">
-        <MovieCard
-          title="וידאו לבדיקה"
-          videoSrc="https://www.w3schools.com/html/mov_bbb.mp4"
-        />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
